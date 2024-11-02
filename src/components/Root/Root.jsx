2e2d7a770970b8751557e86c34f14d493798592d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-02 13:52:21 +0600

    banner created and books data fetched

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from "../NavBar/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
