9fef53e29eae64591f0617aa8c54bd5b164023f7
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-02 13:35:07 +0600

    dynamic and responsive navbar created

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../NavBar/Navbar";
 
 const Root = () => {
     return (
         <div>
-            <h3>Hello from root</h3>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
