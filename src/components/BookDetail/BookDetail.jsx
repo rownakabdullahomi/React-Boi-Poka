1632cd555fb5a840d100b2a7a5e47ac163cb9e0a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-02 18:53:38 +0600

    mark as read done in local storage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDb";
 
 const BookDetail = () => {
+  const data = useLoaderData();
+  const { bookId } = useParams();
+  const id = parseInt(bookId);
 
-    const data = useLoaderData();
-    const {bookId} = useParams();
-    const id = parseInt(bookId);
+  const book = data.find((book) => book.bookId === id);
 
-    const book = data.find(book => book.bookId === id)
+  const { bookId: currentBookId, image } = book;
 
-    const {bookId: currentBookId, image} = book;
+  const handleMarkAsRead = (id) => {
+    addToStoredReadList(id);
+  };
 
-    return (
-        <div className="my-12">
-            <h2>details: {bookId}</h2>
-            <img className='w-36' src={image} alt="" />
-            <br />
-            <button className="btn btn-outline btn-accent mr-5">Read</button>
-            <button className="btn btn-accent">Wish List</button>
-        </div>
-    );
+  return (
+    <div className="my-12">
+      <h2>details: {bookId}</h2>
+      <img className="w-36" src={image} alt="" />
+      <br />
+      <button
+        onClick={() => handleMarkAsRead(bookId)}
+        className="btn btn-outline btn-accent mr-5"
+      >
+        Mark asRead
+      </button>
+      <button className="btn btn-accent">Add to Wish List</button>
+    </div>
+  );
 };
 
 export default BookDetail;
