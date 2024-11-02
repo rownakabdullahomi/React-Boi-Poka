be131a2d70c0b25bcfc1a3554632ca5e7a0a61be
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-02 17:47:45 +0600

    read and wishlist button added

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -6,14 +6,14 @@ const Book = ({ book }) => {
 
   return (
     <Link to={`/books/${bookId}`}>
-      <div className="card bg-base-100 w-96 shadow-xl p-6">
+      <div className="card bg-base-100 w-96 shadow-xl p-6 mx-auto">
         <figure className="bg-blue-200 py-8 rounded-2xl">
           <img className="h-[166px]" src={image} alt={bookName} />
         </figure>
         <div className="card-body">
           <div className="flex justify-center gap-5 ">
-            {tags.map((tag) => (
-              <button className="btn btn-sm bg-green-300 text-green-700">
+            {tags.map((tag, index) => (
+              <button key={index} className="btn btn-sm bg-green-300 text-green-700">
                 {tag}
               </button>
             ))}
--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const BookDetail = () => {
 
+    const data = useLoaderData();
     const {bookId} = useParams();
+    const id = parseInt(bookId);
+
+    const book = data.find(book => book.bookId === id)
+
+    const {bookId: currentBookId, image} = book;
 
     return (
-        <div>
+        <div className="my-12">
             <h2>details: {bookId}</h2>
+            <img className='w-36' src={image} alt="" />
+            <br />
+            <button className="btn btn-outline btn-accent mr-5">Read</button>
+            <button className="btn btn-accent">Wish List</button>
         </div>
     );
 };
