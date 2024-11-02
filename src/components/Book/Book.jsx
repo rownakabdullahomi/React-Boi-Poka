d5d7a88b88c4bde6df975a2447922ce8f9527b81
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-02 16:52:48 +0600

    book card added and designed also data showed

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Book = ({ book }) => {
+  const { image, bookName, author } = book;
+
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl p-6">
+      <figure className="bg-blue-200 py-8 rounded-2xl">
+        <img
+        className="h-[166px]"
+          src= {image}
+          alt={bookName}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {bookName}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>By: {author}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
